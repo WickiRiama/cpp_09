c74f6149132176ceeaffe3f98592c6524256dc1c
Margaux Riant <mriant@student.42.fr>
2023-04-28 17:15:01 +0200

    Add setDatabase and parseDataLine functions
     Changes to be committed:
    	modified:   ex00/includes/BitcoinExchange.hpp
    	new file:   ex00/includes/colors.h
    	modified:   ex00/srcs/BitcoinExchange.cpp

--- ex00/includes/BitcoinExchange.hpp
+++ ex00/includes/BitcoinExchange.hpp
@@ -6,7 +6,7 @@
 /*   By: mriant <mriant@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/26 11:08:47 by mriant            #+#    #+#             */
-/*   Updated: 2023/04/26 11:29:45 by mriant           ###   ########.fr       */
+/*   Updated: 2023/04/28 16:54:32 by mriant           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,13 +20,28 @@ class BitcoinExchange
 {
 public:
 	BitcoinExchange(void);
+	BitcoinExchange(std::string const &file);
 	BitcoinExchange(BitcoinExchange const &src);
 	~BitcoinExchange(void);
 
 	BitcoinExchange &operator=(BitcoinExchange const &rhs);
 
+	void setDatabase(std::string const &file);
+	void parseDataLine(std::string const &line);
+
 private:
 	std::map<std::string, float> database;
+
+public:
+	class WrongDataFormatException : public std::exception
+	{
+	public:
+		virtual const char *what() const throw()
+		{
+			return "Data format must be `date,value`";
+		}
+	};
+
 };
 
 #endif
--- ex00/srcs/BitcoinExchange.cpp
+++ ex00/srcs/BitcoinExchange.cpp
@@ -6,11 +6,15 @@
 /*   By: mriant <mriant@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/26 11:18:10 by mriant            #+#    #+#             */
-/*   Updated: 2023/04/26 11:41:03 by mriant           ###   ########.fr       */
+/*   Updated: 2023/04/28 17:06:34 by mriant           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+#include <fstream>
+
 #include "BitcoinExchange.hpp"
+#include "colors.h"
 
 //==============================================================================
 // Conctructors and Destructor
@@ -20,7 +24,20 @@ BitcoinExchange::BitcoinExchange(void)
 {
 }
 
-BitcoinExchange::BitcoinExchange(BitcoinExchange const &src);
+BitcoinExchange::BitcoinExchange(std::string const &file)
+{
+	try
+	{
+		setDatabase(file);
+	}
+	catch (WrongDataFormatException &e)
+	{
+		std::cout << RED << e.what() << RES << std::endl;
+		database.clear();
+	}
+}
+
+BitcoinExchange::BitcoinExchange(BitcoinExchange const &src)
 {
 	*this = src;
 }
@@ -39,9 +56,43 @@ BitcoinExchange &BitcoinExchange::operator=(BitcoinExchange const &rhs)
 	{
 		this->database = rhs.database;
 	}
-	return this;
+	return *this;
 }
 
 //==============================================================================
 // Functions
 //==============================================================================
+
+void BitcoinExchange::setDatabase(std::string const &file)
+{
+	std::ifstream myfile;
+	std::string line;
+
+	myfile.open(file.c_str());
+	if (myfile.is_open())
+	{
+		while(getline(myfile, line))
+		{
+			try
+			{
+				parseDataLine(line);
+			}
+			catch (WrongDataFormatException &e)
+			{
+				myfile.close();
+				throw WrongDataFormatException();
+			}
+		}
+		myfile.close();
+	}
+}
+
+void BitcoinExchange::parseDataLine(std::string const &line)
+{
+	size_t pos = line.find(',', 0);
+	if (pos == std::string::npos)
+		throw WrongDataFormatException();
+	std::string date = line.substr(0, pos);
+	std::string value = line.substr(pos + 1);
+	database[date] = atoi(value.c_str());
+}
