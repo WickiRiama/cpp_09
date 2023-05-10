ef68c761e476874f4a400a6efed8a7e74b491665
Margaux Riant <mriant@student.42.fr>
2023-05-10 12:11:10 +0200

    Ex0 OK
     Changes to be committed:
    	modified:   srcs/main.cpp

--- ex00/srcs/main.cpp
+++ ex00/srcs/main.cpp
@@ -6,14 +6,22 @@
 /*   By: mriant <mriant@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/26 10:52:54 by mriant            #+#    #+#             */
-/*   Updated: 2023/05/05 13:30:17 by mriant           ###   ########.fr       */
+/*   Updated: 2023/05/10 12:10:09 by mriant           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+
 #include "BitcoinExchange.hpp"
 
-int main(void)
+int main(int ac, char **av)
 {
+	if (ac != 2)
+	{
+		std::cout << "Error: could not open file." << std::endl;
+		return 1;
+	}
 	BitcoinExchange test("data.csv");
-	test.printDatedValues("input.txt");
+	test.printDatedValues(av[1]);
+	return 0;
 }
