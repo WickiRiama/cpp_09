22cfed3ac94259b53bfdb10c56479eeaa5903d95
Margaux Riant <mriant@student.42.fr>
2023-05-11 14:12:08 +0200

    Add definition of canonical form functions
     Changes to be committed:
    	modified:   ex02/includes/PmergeMe.hpp
    	modified:   ex02/srcs/PmergeMe.cpp

--- ex02/includes/PmergeMe.hpp
+++ ex02/includes/PmergeMe.hpp
@@ -6,12 +6,16 @@
 /*   By: mriant <mriant@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/10 19:08:42 by mriant            #+#    #+#             */
-/*   Updated: 2023/05/10 19:11:09 by mriant           ###   ########.fr       */
+/*   Updated: 2023/05/11 13:39:23 by mriant           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PMERGEME_HPP
 #define PMERGEME_HPP
+
+#include <vector>
+#include <list>
+#include <string>
 
 class PmergeMe
 {
@@ -21,6 +25,11 @@ public:
 	~PmergeMe(void);
 
 	PmergeMe &operator=(PmergeMe const &rhs);
+
+	void sort(std::string input);
+
+private:
+	std::vector<int> _input_vec;
 };
 
 #endif
--- ex02/srcs/PmergeMe.cpp
+++ ex02/srcs/PmergeMe.cpp
@@ -1,5 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PmergeMe.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mriant <mriant@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/11 13:33:03 by mriant            #+#    #+#             */
+/*   Updated: 2023/05/11 13:41:45 by mriant           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "PmergeMe.hpp"
+
+//==============================================================================
+// Constructors and Destructor
+//==============================================================================
+
+PmergeMe::PmergeMe(void)
+{
+}
+
+PmergeMe::PmergeMe(PmergeMe const &src)
+{
+	*this = src;
+}
+
+PmergeMe::~PmergeMe(void)
+{
+}
+
+//==============================================================================
+// Operators
+//==============================================================================
+
+PmergeMe &PmergeMe::operator=(PmergeMe const &rhs)
+{
+	if (this != &rhs)
+	{
+		_input_vec = rhs._input_vec;
+	}
+	return *this;
+}
+
+//==============================================================================
+// Functions
+//==============================================================================
+
+void PmergeMe::sort(std::string input)
+{
+// parsing
 // 1 - Former paires
 // 2 - Trier chaque paire
 // 3 - Trier les paires entres elle par plus grand élément > merge sort
 // 4 - Extraire les plus petits éléments des paires
 // 5 - Insérer les plus petits éléments à leur place Insert sort + binary search
+}
